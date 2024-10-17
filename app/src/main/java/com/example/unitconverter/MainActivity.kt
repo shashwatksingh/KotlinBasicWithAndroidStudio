b398a39c247329689853bf7b30144e777de36dbe
shashwatksingh <shashwatksingh@gmail.com>
2024-10-17 19:14:36 +0530

    positioning and spacing of components

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -5,18 +5,27 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,23 +47,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(name: String, modifier: Modifier = Modifier) {
-    Column {
+    Column (
+        modifier= Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         // Stacking UI elements
-        Text(text = "unit converter")
+        Text(text = "unit converter", modifier = Modifier.padding(100.dp))
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {
             // here goes what happens when the value is changed in this outlined text field
         })
         Row {
-            val context = LocalContext.current
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Select")
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow down")
+            }
+            Box {
 
-            Button(onClick = {
-                Toast.makeText(
-                    context,
-                    "Thanks for clicking",
-                    Toast.LENGTH_LONG
-                ).show()
-            }) {
-                Text(text = "Click me!")
             }
         }
     }
